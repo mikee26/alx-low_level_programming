ab9da803b02c88830f95ced7104b329cebedb89b
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-19 01:15:27 -0800

    listing lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - writing lower case letters
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 97; l < 123; l++)
+		putchar(l);
+	return (0);
+}
