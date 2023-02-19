f944b356f9808b33be14a64dd764d8dcb3316924
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-19 05:28:42 -0800

    from 0 - 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - to retrun 10's base digit
+ *
+ * Return: 0 (succsess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 11; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
