c5c150af7b5ddf28d6b8f112079b4efd4050899c
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-19 08:52:04 -0800

    printing without printf and put

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
  * main - printing a text
@@ -8,6 +9,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
