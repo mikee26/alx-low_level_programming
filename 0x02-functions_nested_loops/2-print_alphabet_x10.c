7f679885986ce99c5ffc580309570c587b0d02ed
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-21 11:20:19 -0800

    10 times the alpha bets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing 10x the alpha bet'
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet_x10(void);
+{
+	char l;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		l = 97;
+		while (l < 123)
+		{
+			putchar(l);
+			l++;
+		}
+		putchar('\n');
+		i++;
+	}
+}
