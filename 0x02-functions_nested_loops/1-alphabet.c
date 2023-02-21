df9c34df204bc3a2f3b753d116deb156d14eca61
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-21 09:59:49 -0800

    printing lower case letters with putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - to print letters in lower case followed by new line
+ *
+ * Return: 0 
+ */
+void print_alphabet(void);
+{
+	char l;
+
+	for (l = 97; l <123; l++)
+		_putchar(l);
+	return (0);
+}
