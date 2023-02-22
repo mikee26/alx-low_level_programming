213123af5a019caa38cb6bfc00b08ccb605ba0c8
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-22 12:01:13 -0800

    multiples of 5 and 3

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - computes multple of 5 and 3 and print sum
+ *
+ * Retur: sum of multiples
+ */
+int main(void)
+{
+	int s = 0, n;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 5 || n % 3 == 0)
+		{
+			s += n;
+		}
+	}
+	printf("%d", s);
+	return (0);
+}
+
