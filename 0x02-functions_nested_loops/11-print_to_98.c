80d66aaa97751c1ba67dcac961b63313f32bd3a5
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-22 10:20:53 -0800

    printing until 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_to_98 - displaying until 98 hit
+ * @i: the character
+ * Return: the sequence of numbers
+ */
+int print_to_98(int i)
+{
+	for ( ; i < 99; i++)
+		return (i);
+}
