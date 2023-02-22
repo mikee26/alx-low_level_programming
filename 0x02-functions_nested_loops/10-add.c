f4d6457f527e1b13165b0833ec0c2a84e8ae9e62
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-22 10:10:48 -0800

    addition of numers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - prints the addition of numers
+ * @a: the character
+ * @b: the second character
+ * Return: the addition of a and b
+ */
+int add(int a, int b)
+{
+	int r;
+
+	r = a + b;
+	return (r);
+}
