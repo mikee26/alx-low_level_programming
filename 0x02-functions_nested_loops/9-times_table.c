0bfad663140163f3c6b2955f70e277329984ccd3
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-22 10:04:16 -0800

    the schedule please

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 		for (y = 0; y <= 9; y++)
 		{
 			z = x * y;
-			if (z > y)
+			if (z > 9)
 			{
 				u = z % 10;
 				d = (z - u) / 10;
@@ -30,7 +30,7 @@ void times_table(void)
 					_putchar(32);
 					_putchar(32);
 				}
-				_putchar(z);
+				_putchar(z + '0');
 			}
 		}
 		_putchar('\n');
