25d0cb0bfdcf56677a221ae6d2a0377222d8cc1c
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-22 13:15:10 -0800

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,13 +8,12 @@ int main(void)
 {
 	int i, t1 = 1, t2 = 2, nextTerm;
 
-	for (i = 1; i <= 50; ++i)
+	for (i = 1; i < 50; ++i)
 	{
 		printf("%d, ", t1);
 		nextTerm = t1 + t2;
 		t1 = t2;
 		t2 = nextTerm;
 	}
-	printf("\n");
 	return (0);
 }
