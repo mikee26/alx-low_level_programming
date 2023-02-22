56afb02dc0e8c5fda469a6174d35c67af62d91fe
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-22 05:03:40 -0800

    the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit as the name
+ * @i: the letter
+ * Return: the last digit 0
+ */
+int print_last_digit(int i)
+{
+	int r;
+
+	r = i % 10;
+	return (r);
+}
