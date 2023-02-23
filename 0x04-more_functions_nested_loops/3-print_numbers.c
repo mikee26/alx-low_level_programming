83857cb0a179b933fdf03cf3f02b2c1b4ff2d221
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-23 06:32:30 -0800

    listing til 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - display form 0 to 9
+ *
+ * Return: the numbers
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i >= 48; i < 59; i++)
+		_putchar(i);
+	return (0);
+}
