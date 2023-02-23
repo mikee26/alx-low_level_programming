7ffff5346367477e53d8039caea641b6d34a5e9a
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-23 05:54:57 -0800

    the multiples

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * mul - multiplies numbers
+ * @a: the first numer
+ * @b: the second
+ * Return: the mutiple
+ */
+int mul(int a, int b)
+{
+	r = a * b;
+
+	return (r);
+}
