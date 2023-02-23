ea431f2c3730b9c8ca450a4267afdc02207d98d5
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-23 07:06:03 -0800

    listing til 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,10 +8,10 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i =0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i + '0');
+		putchar(i + '0');
 	}
-	_putchar('\n');
+	putchar('\n');
 }
 
