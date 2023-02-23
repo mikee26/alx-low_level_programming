b7a3cc7d9107fbd97ebef6c6123c8101bf4a93ad
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-23 11:41:59 -0800

    diogonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints diagonal line
+ *
+ * Return: diogonal line
+ */
+int main(void)
+{
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+	return (0);
+}
