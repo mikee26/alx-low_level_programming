d5ae1d259620de3dcf083aa78f0b2596b04cfa6e
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-23 11:50:11 -0800

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,9 +6,16 @@
  */
 int main(void)
 {
-	print_diagonal(0);
-	print_diagonal(2);
-	print_diagonal(10);
-	print_diagonal(-4);
+	int i, j;
+
+	for (i = 1; j <= 10; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('*n');
+	}
+	_putchar('\n');
 	return (0);
 }
