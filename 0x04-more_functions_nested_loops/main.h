e09831fa5c1dc54f4290fba143e72faaa9ea016c
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-23 08:58:45 -0800

    the main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,20 +1,21 @@
-#ifdef MAIN_H
-#define MAIN_H
-/**
- * file -main.h
- * Author - mike
- * Desc: collecton of headers
- */
-
 int _putchar(char c);
+
 int _isupper(int c);
+
 int _isdigit(int c);
+
 int mul(int a, int b);
+
 void print_numbers(void);
+
 void print_most_numbers(void);
+
 void more_numbers(void);
+
 void print_line(int n);
+
 void print_diagonal(int n);
+
 void print_square(int size);
+
 void print_triangle(int size);
-#endif
