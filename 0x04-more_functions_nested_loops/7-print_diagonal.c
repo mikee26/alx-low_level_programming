a993bfd00f199970d5c7f2bf9d448defb3051fdc
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-24 05:45:08 -0800

    diogonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,21 +1,30 @@
 #include "main.h"
-/**
- * main - prints diagonal line
- *
- * Return: diogonal line
- */
-int main(void)
-{
-	int i, j;
 
-	for (i = 1; j <= 10; i++)
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
 	{
-		for (j = 1; j <= i; j++)
-		{
-			_putchar(' ');
-		}
-		_putchar('*n');
+		_putchar('\n');
 	}
-	_putchar('\n');
-	return (0);
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
 }
