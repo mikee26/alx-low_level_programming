5b0156f0d6d7b4aa69b94a2f0d2cc43268d9924e
mikee26 <devaalex500@gmail.comgit config --global user.email devaalex500@gmail.com>
2023-02-24 06:46:59 -0800

    printing any thing as integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,12 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_number - prints an integer
  * @n: integer to be printed
- * Return: 0 success
  */
 void print_number(int n)
 {
-	_putchar(n);
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
